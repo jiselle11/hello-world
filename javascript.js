378a3d3c8221ff67de354ec503b82326fef3de8d
Jiselle McGee <jiselle2002@gmail.com>
2018-11-10 19:35:42 +0000

    add css and fix carousel

--- javascript.js
+++ javascript.js
@@ -1,18 +1,20 @@
 //javascript.js
 
 var slideIndex = 0;
-
+showSlides();
 function showSlides() {
     
-    var slides = document.getElementByClassName('myimages');
-    //var slides = [img1, img2 img3]
+    var slides = document.getElementsByClassName("myimages");
     for (var i = 0; i < slides.length; i++){
         slides[i].style.display= "none";
     }
-    slideIndex++
+    slideIndex++;
     if (slideIndex >= slides.length){
-        slideIndex = 0
+        slideIndex = 0;
     }
+    slides[slideIndex].style.display = "block";
+    setTimeout(showSlides, 4000);
+    
 }
 
 //1st loop: i = 0, slides[i] = img1
